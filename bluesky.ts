a552399edd925240857484246e73c382a27784d1
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2025-08-06 18:45:40 +0200

    optimize code

--- bluesky.ts
+++ bluesky.ts
@@ -2,16 +2,16 @@ import { RichText, AtpAgent } from "@atproto/api";
 import type { Attachment } from "./mastodonTypes.ts";
 import { urlToUint8Array } from "./utils.ts";
 
+const blueskyUrl = Deno.env.get("BLUESKY_ENDPOINT");
+const blueskyHandle = Deno.env.get("BLUESKY_HANDLE");
+const blueskyPassword = Deno.env.get("BLUESKY_PASSWORD");
+
 const loginToBluesky = async (): Promise<AtpAgent> => {
-    const blueskyUrl = Deno.env.get("BLUESKY_ENDPOINT");
     if (!blueskyUrl) throw new Error("BLUESKY_ENDPOINT not set");
+    if (!blueskyHandle) throw new Error("BLUESKY_HANDLE");
+    if (!blueskyPassword) throw new Error("BLUESKY_PASSWORD");
 
     const agent = new AtpAgent({ service: blueskyUrl });
-    const blueskyHandle = Deno.env.get("BLUESKY_HANDLE");
-    if (!blueskyHandle) throw new Error("BLUESKY_HANDLE");
-
-    const blueskyPassword = Deno.env.get("BLUESKY_PASSWORD");
-    if (!blueskyPassword) throw new Error("BLUESKY_PASSWORD");
 
     try {
         const response = await agent.login({
